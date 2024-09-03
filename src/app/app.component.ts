6c782eeaf6bb490cf36374123555e4502b6481ef
dhritisingh18 <dhritisingh3@gmail.com>
2024-09-03 18:01:12 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { TopModule } from './top/top.module';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SellerService } from './services/seller.service';
+import { CommonModule } from '@angular/common';
+import { CartServiceService } from './services/cart-service.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TopModule,HeaderComponent,RouterModule,FormsModule,HttpClientModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private cartService : CartServiceService){}
+  ngOnInit(): void {
+    this.cartService.cartArray$.subscribe((result)=>{
+      
+    })
+  }
   title = 'ecomm-app';
+
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,85 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { NgModule } from '@angular/core';
+import { FourOFourComponent } from './404/404.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
+import { authGuard } from './auth.guard';
+import { SellerLoginComponent } from './seller-login/seller-login.component';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
+import { SearchComponent } from './search/search.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { CartSearchComponent } from './cart-search/cart-search.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"home",
+        component: HomeComponent
+    },
+    {
+        path:"seller-auth",
+        component: SellerAuthComponent
+    }
+    ,
+    {
+        path:"seller-home",
+        component: SellerHomeComponent,
+        canActivate : [authGuard]
+    },
+    {
+        path:"seller-add-product",
+        component: SellerAddProductComponent,
+        canActivate : [authGuard]
+    },
+    {
+        path:"seller-login",
+        component: SellerLoginComponent,
+    }
+    ,
+    {
+        path:"search/:query",
+        component: SearchComponent,
+    },
+    {
+        path:"product/:id",
+        component: ProductDetailComponent,
+    },
+    {
+        path:"user-auth",
+        component: UserAuthComponent,
+    },
+    {
+        path:"user-login",
+        component: UserLoginComponent,
+    }
+    ,
+    {
+        path:"cart",
+        component: CartSearchComponent,
+    }
+    ,
+    {
+        path:"checkout",
+        component: CheckoutComponent    }
+    ,
+    {
+        path:"my-orders",
+        component: MyOrdersComponent    }
+    ,
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {
+        path:"**",
+        component: FourOFourComponent
+    }
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
